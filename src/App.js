62cd24d8bc74b31a5700970e20a849d0ffbe41cc
lukasz <l.bielecki13@gmail.com>
2024-04-09 22:46:14 +0200

    creating Spinner component

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ import { useState, useEffect } from 'react';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
 import { detectFace, updateEntries, checkServerStatus } from './api/requests';
+import { Spinner } from './components/Spinner/Spinner';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [imageUrlResponse, setImageUrlResponse] = useState('');
   const [input, setInput] = useState('');
   const [box, setBox] = useState([]);
@@ -40,8 +42,12 @@ function App() {
   };
 
   useEffect(() => {
+    setIsLoading(true);
     checkServerStatus()
-      .then((data) => console.log(data))
+      .then((data) => {
+        console.log(data);
+        setIsLoading(false);
+      })
       .catch((err) => console.error(err));
   }, []);
 
@@ -113,20 +119,26 @@ function App() {
 
   return (
     <div className='App'>
-      <Navigation isSignedIn={isSignedIn} onRouteChange={handleOnRouteChange} />
-      {route === 'home' ? (
-        <div>
-          <Logo />
-          <Rank name={user.name} entries={user.entries} />
-          <ImageLinkForm onInputChange={handleInputChange} onButtonSubmit={handleOnPictureSubmit} />
-          <FaceRecognition boxes={box} imageUrl={imageUrlResponse} />
-        </div>
-      ) : route === 'signin' ? (
-        <Signin loadUser={handleLoadUser} onRouteChange={handleOnRouteChange} />
+      {isLoading ? (
+        <Spinner />
       ) : (
-        <Register loadUser={handleLoadUser} onRouteChange={handleOnRouteChange} />
+        <>
+          <Navigation isSignedIn={isSignedIn} onRouteChange={handleOnRouteChange} />
+          {route === 'home' ? (
+            <div>
+              <Logo />
+              <Rank name={user.name} entries={user.entries} />
+              <ImageLinkForm onInputChange={handleInputChange} onButtonSubmit={handleOnPictureSubmit} />
+              <FaceRecognition boxes={box} imageUrl={imageUrlResponse} />
+            </div>
+          ) : route === 'signin' ? (
+            <Signin loadUser={handleLoadUser} onRouteChange={handleOnRouteChange} />
+          ) : (
+            <Register loadUser={handleLoadUser} onRouteChange={handleOnRouteChange} />
+          )}
+          <ParticlesBg type='cobweb' bg={true} num={35} />
+        </>
       )}
-      <ParticlesBg type='cobweb' bg={true} num={35} />
     </div>
   );
 }
