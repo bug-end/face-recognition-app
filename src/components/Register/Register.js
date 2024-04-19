0ad253592a1d142ec75ee7c5fc07b516bcc1db2c
lukasz <l.bielecki13@gmail.com>
2024-04-20 00:31:38 +0200

    refactoring Register component

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,5 +1,10 @@
 import { useState } from 'react';
+
+import { Button } from '../Button/Button';
+
 import { registerUser } from '../../api/requests';
+
+import styles from './Register.module.css';
 
 const Register = ({ onRouteChange, loadUser }) => {
   const [email, setEmail] = useState('');
@@ -28,39 +33,57 @@ const Register = ({ onRouteChange, loadUser }) => {
   };
 
   return (
-    <article>
-      <div>
-        <div>
-          <fieldset id='sign_up'>
-            <legend>Register</legend>
-            <div>
-              <label htmlFor='name'>Name</label>
-              <input type='text' name='name' id='name' onChange={onNameChange} />
-            </div>
-            <div>
-              <label htmlFor='email-address'>Email</label>
-              <input type='email' name='email-address' id='email-address' onChange={onEmailChange} />
-            </div>
-            <div>
-              <label htmlFor='password'>Password</label>
-              <input type='password' name='password' id='password' onChange={onPasswordChange} />
-              <div>
-                <p>Password requirements:</p>
-                <ul>
-                  <li>At least 8 characters long</li>
-                  <li>At least one digit</li>
-                  <li>At least one lowercase letter</li>
-                  <li>At least one uppercase letter</li>
-                  <li>At least one special character</li>
-                </ul>
-              </div>
-            </div>
-          </fieldset>
-          <div>
-            <button onClick={onSubmitRegister} type='submit'>
-              Register
-            </button>
+    <article className={styles.wrapper}>
+      <div className={styles.registerBox}>
+        <fieldset id='sign_up' className={styles.fieldset}>
+          <legend className={styles.legend}>Register</legend>
+          <div className={styles.inputWrapper}>
+            <label htmlFor='name' className={styles.label}>
+              Name
+            </label>
+            <input type='text' name='name' id='name' onChange={onNameChange} className={styles.input} />
           </div>
+          <div className={styles.inputWrapper}>
+            <label htmlFor='email-address' className={styles.label}>
+              Email
+            </label>
+            <input
+              type='email'
+              name='email-address'
+              id='email-address'
+              onChange={onEmailChange}
+              className={styles.input}
+            />
+          </div>
+          <div>
+            <div className={styles.inputWrapper}>
+              <label htmlFor='password' className={styles.label}>
+                Password
+              </label>
+              <input
+                type='password'
+                name='password'
+                id='password'
+                onChange={onPasswordChange}
+                className={styles.input}
+              />
+            </div>
+            <div>
+              <p>Password requirements:</p>
+              <ul>
+                <li>At least 8 characters long</li>
+                <li>At least one digit</li>
+                <li>At least one lowercase letter</li>
+                <li>At least one uppercase letter</li>
+                <li>At least one special character</li>
+              </ul>
+            </div>
+          </div>
+        </fieldset>
+        <div>
+          <Button onClick={onSubmitRegister} type='submit'>
+            Register
+          </Button>
         </div>
       </div>
     </article>
